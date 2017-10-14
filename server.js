a692b7ada03ae587bdf0d3d5f2be1945b53d489e
Girish Nuli <>
2017-10-14 11:44:49 -0400

    Home view created

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const winston = require('winston');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const config = require('./config');
+const index = require('./routes/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Static file serving
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Configure template engine to use handlebars
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Add routes
+app.use('/', index);
+
+// Launch the server
+app.listen(config.port, () => {
+  winston.info(`App running on ${config.port}`);
+});
